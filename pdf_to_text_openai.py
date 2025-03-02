3fded53730868a888a527e94664f67a245740a62
GhostPrime1234 <mcmillanbmac@gmail.com>
2025-03-03 10:31:38 +1100

    Remove unused files and update .gitignore to exclude additional directories and file types

--- pdf_to_text_openai.py
+++ pdf_to_text_openai.py
@@ -1,13 +1,13 @@
 import logging
-from os import getenv, cpu_count
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from os import cpu_count, getenv
 from pathlib import Path
-from time import sleep
+from typing import List
 
+import pytesseract
 from openai import OpenAI, OpenAIError
 from pdf2image import convert_from_path
 from PIL import Image
-import pytesseract
-from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
 class LoggerSetup:
@@ -16,9 +16,9 @@ class LoggerSetup:
         """Configures logging for the program."""
         with open(summary_file_path, "w") as log_file:
             log_file.truncate(0)
-        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-        logging.getLogger('pdf2image').setLevel(logging.WARNING)
-        logging.getLogger('openai').setLevel(logging.WARNING)
+        logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+        logging.getLogger("pdf2image").setLevel(logging.WARNING)
+        logging.getLogger("openai").setLevel(logging.WARNING)
 
     @staticmethod
     def log_error(message: str) -> None:
@@ -40,7 +40,7 @@ class OCRProcessor:
     @staticmethod
     def ocr_image(image: Image.Image) -> str:
         """Performs OCR (Optical Character Recognition) on the given image."""
-        return pytesseract.image_to_string(image, lang='eng')
+        return pytesseract.image_to_string(image, lang="eng")
 
     def process_pdf(self, pdf_file: Path) -> str:
         """Processes OCR and generates text from a PDF file."""
@@ -54,8 +54,8 @@ class OCRProcessor:
             if page_range:
                 try:
                     start_str, end_str = page_range.split("-")
-                    convert_kwargs['first_page'] = int(start_str)
-                    convert_kwargs['last_page'] = int(end_str)
+                    convert_kwargs["first_page"] = int(start_str)
+                    convert_kwargs["last_page"] = int(end_str)
                 except Exception as error:
                     LoggerSetup.log_error(f"Invalid range entered ({error}). Processing entire PDF.")
 
@@ -85,9 +85,9 @@ class TextProcessor:
     @staticmethod
     def generate_notes(client: OpenAI, facts: str) -> str:
         """Generates notes by interacting with a chat-based language model."""
-        user_headings = (
-            input("Please enter the headings that you want the notes to be created under separated by a comma: ").split(
-                ","))
+        user_headings = input(
+            "Please enter the headings that you want the notes to be created under separated by a comma: "
+        ).split(",")
         user_headings = ""
         headings_list = [heading.strip() for heading in user_headings]
 
@@ -102,13 +102,15 @@ class TextProcessor:
 
         try:
             with ThreadPoolExecutor() as executor:
-                future = executor.submit(client.chat.completions.create,
-                                         model="gpt-4o-mini",
-                                         temperature=0.6,
-                                         messages=[
-                                             {"role": "system", "content": gpt_input},
-                                             {"role": "user", "content": f"Lecture slides: {facts}"}
-                                         ])
+                future = executor.submit(
+                    client.chat.completions.create,
+                    model="gpt-4o-mini",
+                    temperature=0.6,
+                    messages=[
+                        {"role": "system", "content": gpt_input},
+                        {"role": "user", "content": f"Lecture slides: {facts}"},
+                    ],
+                )
                 response = future.result()
             LoggerSetup.log_info(f"Total tokens used: {response.usage.total_tokens}")
             return response.choices[0].message.content
@@ -129,35 +131,41 @@ class PDFProcessor:
         self.pdf_name = ""
         LoggerSetup.setup_logging(self.SUMMARY_FILE_PATH)
 
+    def list_pdf_files(self) -> List[Path]:
+        """Lists all PDF files in the 'pdf' directory."""
+        return [file for file in self.PDF_DIRECTORY.glob("*.pdf")]
+
     def process_pdf_files(self) -> None:
         """Processes PDF files by OCR and generates summaries."""
         try:
             # Ensure teh summary folder exists
             self.SUMMARY_DIRECTORY.mkdir(parents=True, exist_ok=True)
 
-            for pdf_file in self.PDF_DIRECTORY.glob("*.pdf"):
-                try:
-                    # Input validation
-                    # self.pdf_name = self.prompt_user_for_pdf_name()
-                    # pdf_path = self.PDF_DIRECTORY / self.pdf_name
+            pdf_files = self.list_pdf_files()
+            if not pdf_files:
+                LoggerSetup.log_error("No PDF files found in the 'pdf' directory.")
+                return
 
-                    # Process PDF file
-                    client = OpenAI(api_key=getenv("OPENAI_API_KEY"))
-                    ocr_processor = OCRProcessor(self.MAX_CONCURRENT_TASKS)
-                    # text = ocr_processor.process_pdf(pdf_path)
-                    text = ocr_processor.process_pdf(pdf_file)
+            # Let the user select a file to process
+            selected_pdf = self.prompt_user_for_pdf_selection(pdf_files)
+            try:
+                # Process PDF file
+                client = OpenAI(api_key=getenv("OPENAI_API_KEY"))
+                ocr_processor = OCRProcessor(self.MAX_CONCURRENT_TASKS)
+                # text = ocr_processor.process_pdf(pdf_path)
+                text = ocr_processor.process_pdf(selected_pdf)
 
-                    # Generate notes
-                    text_processor = TextProcessor()
-                    notes = text_processor.generate_notes(client=client, facts=text)
+                # Generate notes
+                text_processor = TextProcessor()
+                notes = text_processor.generate_notes(client=client, facts=text)
 
-                    # Writing to summary file
-                    summary_file = self.SUMMARY_DIRECTORY / f"{pdf_file.stem}_summary.md"
-                    summary_file.write_text(notes, encoding="utf-8")
-                    LoggerSetup.log_info(f"Summary written to: {summary_file}")
-                    # self.SUMMARY_FILE_PATH.write_text(notes, encoding='utf-8')
-                except Exception as inner_error:
-                    LoggerSetup.log_error(f"Error processing PDF {pdf_file}: {inner_error}.")
+                # Writing to summary file
+                summary_file = self.SUMMARY_DIRECTORY / f"{selected_pdf.stem}_summary.md"
+                summary_file.write_text(notes, encoding="utf-8")
+                LoggerSetup.log_info(f"Summary written to: {summary_file}")
+                # self.SUMMARY_FILE_PATH.write_text(notes, encoding='utf-8')
+            except Exception as inner_error:
+                LoggerSetup.log_error(f"Error processing PDF {selected_pdf}: {inner_error}.")
 
         except KeyboardInterrupt:
             LoggerSetup.log_info("Exiting program due to user interruption.")
@@ -166,18 +174,20 @@ class PDFProcessor:
         except Exception as error:
             LoggerSetup.log_error(f"An unexpected error occurred: {error}")
 
-    def prompt_user_for_pdf_name(self) -> str:
-        """Prompts the user to enter the name of the PDF file."""
+    def prompt_user_for_pdf_selection(self, pdf_files: List[str]) -> Path:
+        """Prompts the user to select a PDF file from the list."""
+        for idx, pdf_file in enumerate(pdf_files, 1):
+            print(f"{idx}. {pdf_file.name}")
+
         while True:
-            self.pdf_name = input("Enter the name of the PDF file (e.g., CSIT121.pdf): ")
-            if not self.pdf_name:
-                LoggerSetup.log_error("Please provide a name for the PDF.")
-                sleep(0.1)
+            selection = int(input("Select the number of the file to process: "))
+            if 1 <= selection <= len(pdf_files):
+                return pdf_files[selection - 1]
             else:
-                return self.pdf_name
+                LoggerSetup.log_error(f"Invalid selection. Please enter a number between 1 and {len(pdf_files)}.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         processor = PDFProcessor()
         processor.process_pdf_files()
